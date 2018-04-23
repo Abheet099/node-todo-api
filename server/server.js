04c77bc009a682811e8b8d09ab11d0c72d577a7c
Abheet099 <abheetverma099@gmail.com>
2018-04-24 01:12:00 +0530

    add POST todos and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,64 +1,29 @@
-var mongoose=require('mongoose');
+var express=require('express');
+var bodyParser=require('body-parser');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo=mongoose.model('Todo',{
-    text:{
-        type:String,
-        required:true,
-        minlength:1,
-        trim:true
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    },
-    completedAt:{
-        type:Number,
-        default:null
-    }
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var {user}=require('./models/user');
+
+
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo=new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    })
 });
 
-// var newTodo =new Todo({
-//     text:''
-// });
-//
-// newTodo.save().then((doc)=>{
-//     console.log('saved todo',doc);
-// },(e)=>
-// {
-//     console.log('unable to save todo');
-// });
 
-
-// var otherTodo =new Todo({
-//     text:'Edit this video    '
-// });
-//
-// otherTodo.save().then((doc)=>{
-//     console.log(JSON.stringify(doc,undefined,2));
-// },(e)=>
-// {
-//     console.log('unable to save other todo',e);
-// });
-
-
-var User=mongoose.model('uSer',{
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1
-    }
+app.listen(3000,()=>{
+    console.log('server running on 3000');
 });
-
-var user=new User({
-    email:'abheetverma099@gmail.com'
-});
-
-user.save().then((docs)=>{
-    console.log('user saved',docs);
-},(e)=>{
-    console.log('user not saved',e);
-})
